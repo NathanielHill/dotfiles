0e1555c723230218dd774193fac1c0af68240d4f
Nathaniel Hill <nhill@nathaniel.codes>
2016-04-17 13:30:58 -0500

    Updated fish prompt to work with virtual fish

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -20,7 +20,40 @@ set __fish_git_prompt_show_informative_status 'yes'
 #  set_color normal
 #end
 
+
+function fish_prompt --description 'Write out the prompt'
+# Just calculate this once, to save a few cycles when displaying the prompt
+	if not set -q __fish_prompt_hostname
+		set -g __fish_prompt_hostname (hostname|cut -d . -f 1)
+	end
+
+	set -l color_cwd
+	set -l suffix
+	switch $USER
+		case root toor
+			if set -q fish_color_cwd_root
+				set color_cwd $fish_color_cwd_root
+			else
+				set color_cwd $fish_color_cwd
+			end
+			set suffix '#'
+		case '*'
+			set color_cwd $fish_color_cwd
+			set suffix '>'
+	end
+
+	echo -n -s "$USER" @ "$__fish_prompt_hostname" ' ' (set_color $color_cwd) (prompt_pwd) (set_color normal) "$suffix "
+
+	if set -q VIRTUAL_ENV
+		echo -n -s (set_color -b blue white) "(" (basename "$VIRTUAL_ENV") ")" (set_color normal) " "
+	end
+end
+
+
+
 #set fish_function_path $fish_function_path "/usr/local/lib/python2.7/dist-packages/powerline/bindings/fish"
 #powerline-setup
 #
-set -x EDITOR vim
+	set -x EDITOR vim
+
+eval (python3 -m virtualfish auto_activation global_requirements projects)
