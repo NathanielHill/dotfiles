027ff2002bce0633e5eef6a5dd7c1c49d6021bd3
Nathaniel Hill <nathanielhill@utexas.edu>
2017-07-06 22:12:50 -0500

    New functions for quickly plotting a directory of raw TSV/CSV data! :)

--- config/fish/functions/plotall.fish
+++ config/fish/functions/plotall.fish
@@ -6,13 +6,15 @@ case 0
 case 1
     command gnuplot -e "plot for [file in system('ls *.$argv[1]')] file title file; pause -1"
 case 2
-    command gnuplot -e "set xrange [$argv[2]:*]; plot for [file in system('ls *.$argv[1]')] file title file; pause -1"
+    command gnuplot -e "plot for [file in system('ls *.$argv[1]')] file every ::$argv[2] title file; pause -1"
 case 3
-    command gnuplot -e "set xrange [$argv[2]:$argv[3]]; plot for [file in system('ls *.$argv[1]')] file title file; pause -1"
+    command gnuplot -e "set xrange [$argv[3]:*]; plot for [file in system('ls *.$argv[1]')] file every ::$argv[2] title file; pause -1"
 case 4
-    command gnuplot -e "set xrange [$argv[2]:$argv[3]]; set yrange[$argv[4]:*]; plot for [file in system('ls *.$argv[1]')] file title file; pause -1"
+    command gnuplot -e "set xrange [$argv[3]:$argv[4]]; plot for [file in system('ls *.$argv[1]')] file every ::$argv[2] title file; pause -1"
 case 5
-    command gnuplot -e "set xrange [$argv[2]:$argv[3]]; set yrange[$argv[4]:$argv[5]]; plot for [file in system('ls *.$argv[1]')] file title file; pause -1"
+    command gnuplot -e "set xrange [$argv[3]:$argv[4]]; set yrange[$argv[5]:*]; plot for [file in system('ls *.$argv[1]')] file every ::$argv[2] title file; pause -1"
+case 6
+    command gnuplot -e "set xrange [$argv[3]:$argv[4]]; set yrange[$argv[5]:$argv[6]]; plot for [file in system('ls *.$argv[1]')] file every ::$argv[2] title file; pause -1"
 case '*'
     echo "I don't know what you want! Too many arguments."
 end
