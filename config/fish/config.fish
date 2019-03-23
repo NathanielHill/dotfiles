0c340ed21c170aa429341eb5b0bab4370b53a3e2
Nathaniel Hill <nata@goguna.com>
2019-03-23 12:23:34 -0500

    remove OMF, use agnoster directly

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,11 +1,3 @@
-# Path to Oh My Fish install.
-set -q XDG_DATA_HOME
-  and set -gx OMF_PATH "$XDG_DATA_HOME/omf"
-  or set -gx OMF_PATH "$HOME/.local/share/omf"
-
-# Load Oh My Fish configuration.
-source $OMF_PATH/init.fish
-
 set fish_color_autosuggestion yellow
 
 set -x EDITOR vim
--- config/fish/functions/ls.fish
+++ config/fish/functions/ls.fish
@@ -1,3 +1,4 @@
+# Defined in /home/nhill/.config/fish/functions/ls.fish @ line 1
 function ls
-	env LC_COLLATE=C ls -hl -Isnap --color=always $argv; 
+	env LC_COLLATE=C ls -hl -Isnap --color=always $argv;
 end
