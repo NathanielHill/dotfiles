20d352c4aa6e1769547dedac974dc5f650d3b7fb
Nathaniel Hill <nata@goguna.com>
2019-11-27 19:26:00 -0500

    remove annoying NixOS autoloaded aliases

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -5,3 +5,8 @@ set -x EDITOR vim
 set fish_greeting ""
 
 set --universal fish_user_paths ~/.local/scripts node_modules/.bin ~/.yarn/bin
+
+functions -e l
+functions -e la
+functions -e ll
+functions -e ls
